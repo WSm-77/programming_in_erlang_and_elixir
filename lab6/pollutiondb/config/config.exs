3274a34ea6352d726c169ec63c2829e19c783056
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2025-06-09 15:26:35 +0200

    Play with changesets

--- lab6/pollutiondb/config/config.exs
+++ lab6/pollutiondb/config/config.exs
@@ -1,11 +1,3 @@
-# import Config
-
-# config :pollutiondb, Pollutiondb.Repo,
-#   database: "pollutiondb_repo",
-#   username: "user",
-#   password: "pass",
-#   hostname: "localhost"
-
 import Config
 
 config :pollutiondb,
